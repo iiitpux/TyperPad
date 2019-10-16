cb7f8ee94803e0a0e7a66cc373d7b01af4064d94
Sergey Shaymuratov <iiitpux@mail.ru>
2019-10-16 11:52:25 +0500

    Base projects

---
+++ TyperPad.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TyperPad.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ TyperPad.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TyperPad.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
